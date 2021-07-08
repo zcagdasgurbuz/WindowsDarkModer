bbf6ebc81c31bea65a15cafcd4b96a9599d15735
Zeynel <45305014+zcagdasgurbuz@users.noreply.github.com>
2021-07-08 16:04:51 -0700

    Add files via upload

---
+++ darkmode.bat
@@ -0,0 +1,11 @@
+@echo off
+
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme"') do set value=%%b
+
+if %value% == 0x0 (SET decision=1) else (SET decision=0)
+
+REM this will make the apps use dark mode
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /f /v "AppsUseLightTheme" /t REG_DWORD /d %decision%
+REM thiw will also switch windows theme
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /f /v "SystemUsesLightTheme" /t REG_DWORD /d %decision%
+
---
+++ darkmodeonlyapps.bat
@@ -0,0 +1,8 @@
+@echo off
+
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme"') do set value=%%b
+
+if %value% == 0x0 (SET decision=1) else (SET decision=0)
+
+REM this will make the apps use dark mode
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /f /v "AppsUseLightTheme" /t REG_DWORD /d %decision%
